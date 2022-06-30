6fa308f5a86252363f7bfd232ae9b4aa82c57377
Shankar Samidala <samidalashankar@outlook.com>
2022-06-30 22:22:25 +0530

    init

--- app/app.py
+++ app/app.py
@@ -1,72 +1,79 @@
 import os
-from flask import Flask, redirect, render_template, request
-from PIL import Image
-import torchvision.transforms.functional as TF
-import CNN
+import tensorflow as tf
 import numpy as np
-import torch
-import pandas as pd
+from tensorflow import keras
+from skimage import io
+from tensorflow.keras.preprocessing import image
 
 
-disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
-supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
 
-model = CNN.CNN(39)    
-model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
-model.eval()
-
-def prediction(image_path):
-    image = Image.open(image_path)
-    image = image.resize((224, 224))
-    input_data = TF.to_tensor(image)
-    input_data = input_data.view((-1, 3, 224, 224))
-    output = model(input_data)
-    output = output.detach().numpy()
-    index = np.argmax(output)
-    return index
-
-
+# Define a flask app
 app = Flask(__name__)
 
-@app.route('/')
-def home_page():
-    return render_template('home.html')
+# Model saved with Keras model.save()
 
-@app.route('/contact')
-def contact():
-    return render_template('contact-us.html')
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
 
-@app.route('/index')
-def ai_engine_page():
+model =tf.keras.models.load_model('PlantDNet.h5',compile=False)
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, grayscale=False, target_size=(64, 64))
+    show_img = image.load_img(img_path, grayscale=False, target_size=(64, 64))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = np.array(x, 'float32')
+    x /= 255
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
     return render_template('index.html')
 
-@app.route('/mobile-device')
-def mobile_device_detected_page():
-    return render_template('mobile-device.html')
 
-@app.route('/submit', methods=['GET', 'POST'])
-def submit():
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
     if request.method == 'POST':
-        image = request.files['image']
-        filename = image.filename
-        file_path = os.path.join('static/uploads', filename)
-        image.save(file_path)
-        print(file_path)
-        pred = prediction(file_path)
-        title = disease_info['disease_name'][pred]
-        description =disease_info['description'][pred]
-        prevent = disease_info['Possible Steps'][pred]
-        image_url = disease_info['image_url'][pred]
-        supplement_name = supplement_info['supplement name'][pred]
-        supplement_image_url = supplement_info['supplement image'][pred]
-        supplement_buy_link = supplement_info['buy link'][pred]
-        return render_template('submit.html' , title = title , desc = description , prevent = prevent , 
-                               image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
+        # Get the file from post request
+        f = request.files['file']
 
-@app.route('/market', methods=['GET', 'POST'])
-def market():
-    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
-                           supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        print(preds[0])
+
+        # x = x.reshape([64, 64]);
+        disease_class = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight',
+                         'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight',
+                         'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
+                         'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
+                         'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
+        a = preds[0]
+        ind=np.argmax(a)
+        print('Prediction:', disease_class[ind])
+        result=disease_class[ind]
+        return result
+    return None
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(port=5002, debug=True)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
+    app.run()
